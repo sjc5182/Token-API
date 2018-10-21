1f586492ecf0873bf94231b0e362931a8759d468
John Chen <shiguangchen1987@gmail.com>
2018-10-21 00:58:01 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import GitHub from './Github.js';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <GitHub></GitHub>
+      </div>
+    );
+  }
+}
+export default App;
